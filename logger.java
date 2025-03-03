b034667beefbfca63122b6c5f6b40097bd39d8fb
luis jaimes <jaimesluis263@gmail.com>
2025-03-03 15:04:16 -0600

    first commit

---
+++ logger.java
@@ -0,0 +1,43 @@
+import java.util.*; 
+
+
+public class logger {
+
+   
+
+
+
+
+    public static void main(String[] args) {
+        string test ="START logging started ";
+        Arraylist<String>logger= new ArrayList<String>();;
+    
+        Date d1 = new Date(); 
+
+        System.out.println("current time "+d1);
+        reader(test);
+
+
+    }
+
+
+
+
+    public String reader(String s){
+        string yo= s;
+
+
+        for (int i:s){
+            system.println(i);
+        }
+
+
+
+    
+        return yo;    
+    }
+    
+
+
+
+}
