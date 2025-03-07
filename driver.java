09300a1cc8e068d55c600e633737275791a4f7bc
luis jaimes <jaimesluis263@gmail.com>
2025-03-06 20:04:35 -0600

    added stuff to devlog

--- driver.java
+++ driver.java
@@ -7,20 +7,22 @@ import java.io.*;
 public class driver {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        proccCreator();
+        
         
         
                 String input = scanner.nextLine();
+                proccCreator(input);
+
         
         
-                System.out.println(input);
+                //System.out.println(input);
             }
         
         
         
         
         
-            public static void proccCreator()
+            public static void proccCreator(String sendData)
     {
         try {
             ProcessBuilder logger = new ProcessBuilder("java", "logger");
@@ -31,22 +33,22 @@ public class driver {
 
             // Send data 
             PrintWriter writer = new PrintWriter(loggerOutput, true);
-            writer.println("Hello logger ");
+            writer.println(sendData);
             writer.println("STOP");
 
             
 
-            // Read data 
+             // Read data 
             BufferedReader reader = new BufferedReader(new InputStreamReader(loggerInput));
             String input;
             while ((input = reader.readLine()) != null) {
-                System.out.println("Parent received " + input);
+               System.out.println(" " + input);
                 
                 if (input.equals("STOP")) 
                 break;
                 
             }
-
+                
             // Wait 
             loggerProcess.waitFor();
 
--- logger.java
+++ logger.java
@@ -7,25 +7,27 @@ import java.util.*;
 
 public class logger {
 
-   
+    static ArrayList<String>log= new ArrayList<String>();
+
 
 
 
 
     public static void main(String[] args) {
+        try {
+        
         loggerProcc();//made static maybe not?
-                String test ="START logging started ";
-                String test2 ="ENCRYPT HELLO ";
+                //String test ="START logging started ";
+                //String test2 ="ENCRYPT HELLO ";
         
-                ArrayList<String>log= new ArrayList<String>();
-                log.add(reader(test));
-                log.add(reader(test2));
+               // ArrayList<String>log= new ArrayList<String>();
+                //log.add(reader(test));
+                //log.add(reader(test2));
         
-                System.out.println(log);
-                
-        
-                
-                
+                System.out.println(log+"print log");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
                     }
                 
                 
@@ -74,23 +76,36 @@ public class logger {
         
         
         
-            public static void loggerProcc()
-    {
-        try { 
+
+
+    public static void loggerProcc()throws IOException {//need to save the input from this
+
+        
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String input;
+        String testinput=" ";
 
         while ((input = reader.readLine()) != null) {
+
             if (input.equals("STOP")) 
                 break; 
             
             System.out.println("Child received from Parent: " + input);
+            testinput=input;
         }
 
+        System.out.println();
+
+        //System.out.println(testinput+"this is where it is");
+        log.add(reader(testinput));
+       //System.out.println(log+"print log");
+
+        
+
         // Send data 
-        PrintWriter writer = new PrintWriter(System.out, true);
-        writer.println("hello driver");
-        writer.println("STOP");
+       // PrintWriter writer = new PrintWriter(System.out, true);
+        //writer.println("hello driver");
+        //writer.println("STOP");
 
 
 
@@ -98,10 +113,8 @@ public class logger {
 
 
         reader.close();
-        writer.close();
-    } catch (IOException e) {
-        e.printStackTrace();
-    }
+        //writer.close();
+    
 }
     }
 
