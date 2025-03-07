e400b0ca8c923d939a916c9a753143c1c3b28f40
luis jaimes <jaimesluis263@gmail.com>
2025-03-06 19:10:12 -0600

    added stuff to devlog after rough draft of logger and driver comunicating with one another

--- driver.java
+++ driver.java
@@ -1,5 +1,72 @@
+import java.util.Scanner;
+import java.io.*;
+
+
+
+
 public class driver {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
-    }
+        Scanner scanner = new Scanner(System.in);
+        proccCreator();
+        
+        
+                String input = scanner.nextLine();
+        
+        
+                System.out.println(input);
+            }
+        
+        
+        
+        
+        
+            public static void proccCreator()
+    {
+        try {
+            ProcessBuilder logger = new ProcessBuilder("java", "logger");
+            Process loggerProcess = logger.start();
+
+            InputStream loggerInput = loggerProcess.getInputStream();
+            OutputStream loggerOutput = loggerProcess.getOutputStream();
+
+            // Send data 
+            PrintWriter writer = new PrintWriter(loggerOutput, true);
+            writer.println("Hello logger ");
+            writer.println("STOP");
+
+            
+
+            // Read data 
+            BufferedReader reader = new BufferedReader(new InputStreamReader(loggerInput));
+            String input;
+            while ((input = reader.readLine()) != null) {
+                System.out.println("Parent received " + input);
+                
+                if (input.equals("STOP")) 
+                break;
+                
+            }
+
+            // Wait 
+            loggerProcess.waitFor();
+
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    } 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- logger.java
+++ logger.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.util.*; 
 
 
@@ -9,63 +13,105 @@ public class logger {
 
 
     public static void main(String[] args) {
-        String test ="START logging started ";
-        String test2 ="ENCRYPT HELLO ";
-
-        ArrayList<String>log= new ArrayList<String>();
-        log.add(reader(test));
-        log.add(reader(test2));
-
-        System.out.println(log);
+        loggerProcc();//made static maybe not?
+                String test ="START logging started ";
+                String test2 ="ENCRYPT HELLO ";
         
-
+                ArrayList<String>log= new ArrayList<String>();
+                log.add(reader(test));
+                log.add(reader(test2));
+        
+                System.out.println(log);
+                
+        
+                
+                
+                    }
+                
+                
+                
+                
+                public static String reader(String s){
+        
+                    String action ="[";
+                    String rest =" ";
+                    Date date = new Date(); 
+                    String time= date.toString();
+                    int firstblank=0;
+        
+                    for (int i =0;i<s.length();i++){
+                    char ch = s.charAt(i);
+                    
+                    if (ch!=' '&& firstblank==0)
+                    action+=ch;	
+                    else if (firstblank==0) {
+                        firstblank++;
+                        action+=']';
+                    }
+                    
+                    
+                    if(firstblank==1)
+                    rest+=ch;
+                    
         
         
+        
+                }
+                
+        
+               
+                String fullstr="";
+                fullstr=time+action+rest;
+                //System.out.println(fullstr);
+        
+        
+        
+            
+                return fullstr;    
             }
         
         
         
         
-        public static String reader(String s){
+        
+            public static void loggerProcc()
+    {
+        try { 
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String input;
 
-        	String action ="[";
-        	String rest =" ";
-            Date date = new Date(); 
-            String time= date.toString();
-            int firstblank=0;
-
-            for (int i =0;i<s.length();i++){
-            char ch = s.charAt(i);
+        while ((input = reader.readLine()) != null) {
+            if (input.equals("STOP")) 
+                break; 
             
-            if (ch!=' '&& firstblank==0)
-            action+=ch;	
-            else if (firstblank==0) {
-            	firstblank++;
-            	action+=']';
-            }
-            
-            
-            if(firstblank==1)
-            rest+=ch;
-            
-
-
-
+            System.out.println("Child received from Parent: " + input);
         }
-        
 
-       
-        String fullstr="";
-        fullstr=time+action+rest;
-        //System.out.println(fullstr);
-
+        // Send data 
+        PrintWriter writer = new PrintWriter(System.out, true);
+        writer.println("hello driver");
+        writer.println("STOP");
 
 
-    
-        return fullstr;    
+
+
+
+
+        reader.close();
+        writer.close();
+    } catch (IOException e) {
+        e.printStackTrace();
     }
+}
+    }
+
+
+
+
+
+
     
 
 
 
-}
+
