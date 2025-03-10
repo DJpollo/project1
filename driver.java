e588cdfb5b2a29ec1043c28097aba628caf727c7
luis jaimes <jaimesluis263@gmail.com>
2025-03-10 17:05:09 -0500

    added to devlog

--- driver.java
+++ driver.java
@@ -23,15 +23,57 @@ public class driver {
 
 
         while (true) {
+           
+
+            
+
+            System.out.println("--------------------------------------");
+            System.out.println("              MENU                     ");
+            System.out.println("--------------------------------------");
+            System.out.println("password - Set password");
+            System.out.println("encrypt - encrypt your input");
+            System.out.println("decrypt - decrypt a string");
+            System.out.println("history - show history ");
+            System.out.println("quit - end program");
+            System.out.println("--------------------------------------");
+            System.out.print("Enter command- ");
+
+
             String input = scanner.nextLine();  
-            if ("END".equals(input)) {
-                writer.println(input); 
-                writer.flush();
-                break; // Exit loop
+
+
+            
+            
+            
+            if (input.equals("quit"))
+            {
+              loggerProcess.destroy();
+               break; // Exit loop
+               
             }
+           
 
             writer.println(input); // Send input to logger 
             writer.flush();
+            
+
+
+
+
+            if(input.equals("password"))
+                System.out.println("password");
+            else if (input.equals("encrypt"))
+                System.out.println("encrypt");
+            else if (input.equals("decrypt"))
+                System.out.println("decrypt");
+            else if (input.equals("history"))
+                System.out.println("history");
+            else 
+                System.out.println("enter valid input");
+   
+            
+
+
 
 
 
